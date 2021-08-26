fc083c5f126c8e397c25d2fcd11c165de533d0f6
narain2992 <60614362+narain2992@users.noreply.github.com>
2021-08-26 16:02:04 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,108 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags = var.tags
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  count               = "${var.node_count}"
+  name                = "${var.prefix}-${format("%02d", count.index)}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+   tags = {
+      dept = "Azure Dev Ops",
+      task = "Image deployment"
+      environment = "Production"
+  }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "PublicIPForLB"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  tags = var.tags
+}
+
+resource "azurerm_lb" "main" {
+  name                = "AZProj02LoadBalancer"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+  tags = var.tags
+}
+
+
+data "azurerm_resource_group" "image" {
+  name                = var.packer_resource_group_name
+}
+
+data "azurerm_image" "image" {
+  name                = var.packer_image_name
+  resource_group_name = data.azurerm_resource_group.image.name
+}
+
+
+resource "azurerm_linux_virtual_machine" "main" {
+  count                           = "${var.node_count}"
+  name                            = "${var.prefix}-vm-${format("%02d", count.index)}"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "${var.admin_user}"
+  admin_password                  = "${var.admin_password}"
+  disable_password_authentication = false
+  network_interface_ids = [
+    element(azurerm_network_interface.main.*.id, count.index)
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_availability_set" "main" {
+  name                = "azPrj_availability_set"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tags = var.tags
+}
